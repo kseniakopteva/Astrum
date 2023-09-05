39b2c5ade575e420c40972db843c5803058b81c8
kseniakopteva <ksenija.kopteva@gmail.com>
2023-09-05 16:21:31 +0300

    deleted old files before breeze

--- resources/views/help.blade.php
+++
@@ -1,3 +0,0 @@
-<x-layout>
-    <h1 class="large-title">Help</h1>
-</x-layout>
--- resources/views/note.blade.php
+++
@@ -1,9 +0,0 @@
-<x-layout>
-    <article class="note">
-        {{ $note->body }}
-        <div class="note-footer">
-            by <a href="/u/{{ $note->author->username }}">{{ $note->author->username }}</a>
-            {{ $note->created_at->diffForHumans() }}
-        </div>
-    </article>
-</x-layout>
