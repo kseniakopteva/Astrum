d2f9c7774681023567bf59a8e2974709f9c9c97c
kseniakopteva <ksenija.kopteva@gmail.com>
2023-11-20 14:48:02 +0200

    help page, watermark and post frame guidelines

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -1,5 +1,120 @@
 <x-main-layout>
-    <div class="wrapper">
-        <h1 class="large-title">Help</h1>
+    <div class="bg-white dark:bg-neutral-800 max-w-7xl m-auto min-h-[calc(100vh-9rem)] p-6">
+        <h1 class="large-title mb-4">Help</h1>
+        <p class="text-lg">Hello! hopefully this page will guide you through Astrum.</p>
+        <ul class="m-6 space-y-12">
+            <li>
+                <h2 id="post-watermark-options" class="text-2xl mb-3 bg-neutral-200 dark:bg-neutral-700 p-2">Post Watermark Options</h2>
+
+                <div class="grid grid-cols-3 gap-5">
+                    <div>
+                        <h3 class="text-lg">Center</h3>
+                        <img src="{{ asset('images/watermark_center.jpg') }}" alt="">
+                    </div>
+                    <div>
+                        <h3 class="text-lg">Tiled</h3>
+                        <img src="{{ asset('images/watermark_tiled.jpg') }}" alt="">
+                    </div>
+                    <div>
+                        <h3 class="text-lg">Bottom right</h3>
+                        <img src="{{ asset('images/watermark_bottomright.jpg') }}" alt="">
+                    </div>
+                </div>
+            </li>
+
+            <li>
+                <h2 id="post-frame-upload-guidelines" class="text-2xl mb-3 bg-neutral-200 dark:bg-neutral-700 p-2">Post Frame Upload Guidelines</h2>
+                <p>Let's assume you want to upload this post frame:</p>
+                <img src="{{ asset('images/pf_guide_1.png') }}" alt="" class="w-28 h-28 m-8">
+                <h3 class="text-lg my-2 font-bold">Percentage</h3>
+                <p>We count the parts of the frame according to this picture:</p>
+                <img id="img" src="{{ asset('images/pf_guide_2.png') }}" alt="" class="w-72 my-3">
+                <p>And so the 'Percentage' will be 12.5. The width is how large it will be:</p>
+                <h3 class="text-lg my-2 font-bold">Width</h3>
+                <p class="mb-3">First is Width: 10, the second 20.</p>
+                <div class="grid grid-cols-2 gap-10">
+                    <article style="border-image: url('{{ asset('images/pf_guide_1.png') }}') 12.5% round;
+                    border-style: solid; border-width: 10px !important;"
+                        class="m-5 first-letter:flex flex-col justify-between border bg-white border-neutral-200 dark:bg-neutral-800 dark:border-neutral-700 rounded-lg p-4 pt-4 mb-4 break-inside-avoid">
+                        <div class="">
+                            <a class="">
+                                <div class="flex justify-between items-center">
+                                    <h1 class="medium-title mr-2">
+                                        Title
+                                    </h1>
+                                    <span>2 hours ago</span>
+                                </div>
+                            </a>
+                            <h2 class="mb-2">
+                                <a>
+                                    johndoe
+                                </a>
+                            </h2>
+                            <img class="w-full" src="" alt="">
+
+                            <div>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum deserunt iste quos consequatur
+                                doloremque? Obcaecati excepturi earum voluptatum? Quia mollitia odit saepe, et eveniet blanditiis
+                                eum laboriosam debitis provident numquam?
+                            </div>
+                        </div>
+                        <div class="">
+                            <footer class="mt-4 flex justify-between">
+                                <a class="space-x-1"><span>5</span><i class="fa-regular fa-comment"></i></a>
+
+                                <x-secondary-button type="button" class="space-x-0.5 pointer-events-none">
+                                    <span>206</span>
+                                    <i class="fa-regular fa-heart"></i>
+                                </x-secondary-button>
+                            </footer>
+                        </div>
+                    </article>
+                    <article style="border-image: url('{{ asset('images/pf_guide_1.png') }}') 12.5% round;
+                border-style: solid; border-width: 20px !important;"
+                        class="m-5 first-letter:flex flex-col justify-between border bg-white border-neutral-200 dark:bg-neutral-800 dark:border-neutral-700 rounded-lg p-4 pt-4 mb-4 break-inside-avoid">
+                        <div class="">
+                            <a class="">
+                                <div class="flex justify-between items-center">
+                                    <h1 class="medium-title mr-2">
+                                        Title
+                                    </h1>
+                                    <span>2 hours ago</span>
+                                </div>
+                            </a>
+                            <h2 class="mb-2">
+                                <a>
+                                    johndoe
+                                </a>
+                            </h2>
+                            <img class="w-full" src="" alt="">
+
+                            <div>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum deserunt iste quos consequatur
+                                doloremque? Obcaecati excepturi earum voluptatum? Quia mollitia odit saepe, et eveniet blanditiis
+                                eum laboriosam debitis provident numquam?
+                            </div>
+                        </div>
+                        <div class="">
+                            <footer class="mt-4 flex justify-between">
+                                <a class="space-x-1"><span>5</span><i class="fa-regular fa-comment"></i></a>
+
+                                <x-secondary-button type="button" class="space-x-0.5 pointer-events-none">
+                                    <span>206</span>
+                                    <i class="fa-regular fa-heart"></i>
+                                </x-secondary-button>
+                            </footer>
+                        </div>
+                    </article>
+                </div>
+            </li>
+        </ul>
     </div>
+
+    <script>
+        (changeImg = function() {
+            if (localStorage.getItem("theme") === 'dark') {
+                document.querySelector('#img').src = '{{ asset('images/pf_guide_2_dark.png') }}'
+            }
+        })()
+    </script>
 </x-main-layout>
--- resources/views/starshop/post-frames/create.blade.php
+++ resources/views/starshop/post-frames/create.blade.php
@@ -1,13 +1,11 @@
 <x-starshop-layout>
     <div class="max-w-xl m-auto">
         <h2 class="medium-title mb-6">Submit a Post Frame</h2>
-        <form action="{{ route('starshop.post-frames.store') }}" method="POST" class="space-y-2"
-            enctype="multipart/form-data">
+        <form action="{{ route('starshop.post-frames.store') }}" method="POST" class="space-y-2" enctype="multipart/form-data">
             @csrf
             <div>
                 <x-input-label for="name">Name</x-input-label>
-                <x-text-input id="name" name="name" class="w-full" required
-                    value="{{ old('name') }}"></x-text-input>
+                <x-text-input id="name" name="name" class="w-full" required value="{{ old('name') }}"></x-text-input>
                 <x-input-error :messages="$errors->get('name')" class="mt-2" />
             </div>
 
@@ -16,16 +14,35 @@
 
             <div>
                 <x-input-label for="width">Width</x-input-label>
-                <input type="number" name="width" id="width" value="{{ old('width') }}"
-                    class="block border-neutral-300 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-300 focus:border-lime-500 dark:focus:border-lime-600 focus:ring-lime-500 dark:focus:ring-lime-600 rounded-md">
-                <x-input-error :messages="$errors->get('width')" class="mt-2" />
+                <div class="flex items-center gap-5">
+                    <input type="number" name="width" id="width" value="{{ old('width') }}"
+                        class="block border-neutral-300 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-300 focus:border-lime-500 dark:focus:border-lime-600 focus:ring-lime-500 dark:focus:ring-lime-600 rounded-md">
+                    <x-input-error :messages="$errors->get('width')" class="mt-2" />
+                    <a target="_blank" href="{{ route('help') . '#post-frame-upload-guidelines' }}" class="question-icon-link">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1.4em"
+                            viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                            <path
+                                d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm169.8-90.7c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
+                        </svg>
+                    </a>
+                </div>
             </div>
 
             <div>
                 <x-input-label for="percentage">Percentage of image for frame</x-input-label>
-                <input type="number" step="0.01" name="percentage" id="percentage" value="{{ old('percentage') }}"
-                    class="block border-neutral-300 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-300 focus:border-lime-500 dark:focus:border-lime-600 focus:ring-lime-500 dark:focus:ring-lime-600 rounded-md">
-                <x-input-error :messages="$errors->get('percentage')" class="mt-2" />
+                <div class="flex items-center gap-5">
+                    <input type="number" step="0.01" name="percentage" id="percentage" value="{{ old('percentage') }}"
+                        class="block border-neutral-300 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-300 focus:border-lime-500 dark:focus:border-lime-600 focus:ring-lime-500 dark:focus:ring-lime-600 rounded-md">
+                    <x-input-error :messages="$errors->get('percentage')" class="mt-2" />
+                    <a target="_blank" href="{{ route('help') . '#post-frame-upload-guidelines' }}" class="question-icon-link">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1.4em"
+                            viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                            <path
+                                d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm169.8-90.7c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
+                        </svg>
+                    </a>
+                </div>
+
             </div>
 
             <div>
@@ -43,8 +60,7 @@
 
             <div class="pb-2">
                 <x-input-label for="tags">Tags</x-input-label>
-                <x-text-input class="h-auto" id="tags" class="block w-full" type="text" name="tags"
-                    placeholder="Tags (separate with comma)" value="{{ old('tags') }}" />
+                <x-text-input class="h-auto" id="tags" class="block w-full" type="text" name="tags" placeholder="Tags (separate with comma)" value="{{ old('tags') }}" />
                 <x-input-error :messages="$errors->get('tags')" class="mt-2" />
             </div>
 
